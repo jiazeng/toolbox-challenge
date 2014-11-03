1c29432765445720919d065d4b3fd4819ef58014
jiazeng <sophbj93@uw.edu>
2014-11-03 00:42:28 -0800

    finish game play (error: flipping back)

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ toolbox-challenge
 =================
 
 Assets and file stubs for the Expanding Your Toolbox Challenge
+Using the jQuery library to catch events and manipulate the DOM
+Using the Lo-Dash library utility functions, such as randomly shuffling an array
+Translating general rules and requirements into algorithms and data structures
+Creating an engaging gaming experience
+Balancing the realities of a deadline with the endless possibilities of web UI (i.e., learn to stop and move on when it's good enough)
--- js/app.js
+++ js/app.js
@@ -4,6 +4,13 @@
 var tiles = [];
 var idx;
 
+var firstTile = null;
+var firstImg;
+var secondTile;
+var resetting = null;
+var matchedCount = 0;
+var missedCount = 0;
+
 for(idx = 1; idx <= 32; ++idx) {
     tiles.push ({
         tileNum: idx,
@@ -15,10 +22,17 @@ for(idx = 1; idx <= 32; ++idx) {
 
 //console.log(tiles);
 
-//when document is rea...
+//when document is ready...
+//Shuffle the array, get 8, and cloned them into a set of 16
 $(document).ready(function() {
+    $('#instruction').popover();
+
     //catch click event of start game button
     $('#start-game').click(function() {
+
+        $('#gameFinish').css('display', 'none');
+        $('#game-board').fadeIn(300);
+
         console.log('start game button clicked!');
         tiles = _.shuffle(tiles);
         var selectedTiles = tiles.slice(0,8);
@@ -28,7 +42,6 @@ $(document).ready(function() {
             tilePairs.push(_.clone(tile));
         });
         tilePairs = _.shuffle(tilePairs);
-        console.log(tilePairs);
 
         var gameBoard = $('#game-board');
         var row = $(document.createElement('div'));
@@ -51,19 +64,69 @@ $(document).ready(function() {
 
         //get starting milliseconds
         var startTime = Date.now();
-        window.setInterval(function() {
+        var timer = window.setInterval(function() {
             var elapsedSeconds = (Date.now() - startTime) / 1000;
             elapsedSeconds = Math.floor(elapsedSeconds);
             $('#elapsed-seconds').text(elapsedSeconds + ' seconds');
+            $('#matchedCount').text(matchedCount);
+            $('#missedCount').text(missedCount);
         }, 1000);
 
-
+        //click on the image
         $('#game-board img').click(function() {
             console.log(this.alt);
             var clickedImg = $(this);
             var tile = clickedImg.data('tile');
             console.log(tile);
-            flipTile(tile, clickedImg);
+
+            if(tile.flipped || tile.matched || resetting) {
+                return;
+            }
+            if (!firstTile) { //if it is the first click, remember the tile and the picture
+
+                console.log("first click");
+                firstTile = tile;
+                firstImg = $(this);
+                //firstTile = firstImg.data('tile');
+                firstTile = tile;
+                flipTile(firstTile, firstImg);
+                console.log("firstTile: " + firstTile);
+
+            } else { //if it is the second click
+                console.log("second click");
+                flipTile(tile, clickedImg);
+                //secondTile = $(this).data('tile');
+                secondTile = tile;
+                console.log("second tile" + secondTile);
+
+                if(firstTile.tileNum == tile.tileNum) { //if two tiles match
+                    console.log(firstTile.tileNum + "==" + tile.tileNum);
+                    matchedCount++;
+                    firstTile.matched = true;
+                    secondTile.matched = true;
+
+                } else { //tiles don't match
+                    console.log("tiles don't match");
+                    missedCount++;
+                    resetting = window.setTimeout(function() {
+                        flipTile(tile, clickedImg);
+                        flipTile(firstTile, firstImg);
+                    }, 1000);
+                    resetting = null;
+                }
+                firstTile = null;
+                secondTile = null;
+            }
+
+            if(matchedCount == 8) { //game finishes
+                //timeout
+                window.clearInterval(timer);
+
+                //show states;
+                //$('#win-screen').fadeIn(300);
+                //$('#resetButton').fadeIn(300);
+                //$('#audio')[0].play();
+            }
         });
 
     }); //start game button click
@@ -72,15 +135,15 @@ $(document).ready(function() {
 
 function flipTile(tile, img) {
     img.fadeOut(100, function() {
-        if(tile.flipped) {
+        if(tile.flipped) { //if flipped, show back
             img.attr('src', 'img/tile-back.png');
         }
-        else {
+        else { //if not flipped, show picture
             img.attr('src', tile.src);
         }
-        tile.flipped = !tile.flipped;
+        tile.flipped = !tile.flipped; //record if this picture is flipped or not
         img.fadeIn(100);
-    });
+    }); //img fade out
 }
 
 /*
@@ -100,10 +163,29 @@ if already matched or clicked, if flipped == true, just return
 
 second move (null) flipped/null
 
+ if there are two tiles flipped over:
+ if the first tile has the same face as the second tile:
+ keep the tiles face up
+
+
 if(matches == 8 || remaining == 0) {
     Congrats!
 }
  */
+
+/*
+4 variales higher than click event
+
+track which image was clicked on, and it's tile to compare to last one
+
+clear the variable using to track first and last image
+
+won game when matches ==8 or remain = 0
+
+get another variable to decide when to ignore your click, boolean resetting outside click handler
+
+reset to true, and then set to false. inside click handler, if the resetting is true then just return
+*/
 
 /*
 four attributes,
@@ -118,6 +200,5 @@ boolean resetting (outside click handler), set to true
 after set to false
 if(resetting = true) {
     just return;
-}
-
+ }
 */
